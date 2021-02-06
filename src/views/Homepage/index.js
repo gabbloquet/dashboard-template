d520d7ce06e0a9b6c8e5f7acd0829bf100e9f4a3
gabin.bloquet <gabin.bloquet@decathlon.com>
2021-02-06 22:59:13 +0100

    style: little update (center p)

--- src/views/Homepage/index.js
+++ src/views/Homepage/index.js
@@ -7,6 +7,7 @@ const Homepage = () => {
     <section className='homepage'>
       <Topbar componentName='Homepage'/>
       <h1>Welcome ! This is the Homepage !</h1>
+      <p>Start to create content easily :)</p>
     </section>
   );
 };
